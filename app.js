d9432d2c7030fb9d5404a8816cab4f395885560b
mhgarry92 <mhgarry92@gmail.com>
2024-01-14 01:12:29 -0500

    basic express setup

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const PORT = process.env.PORT || 8888;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from server');
+    res.status(200).json({message: 'Hello from the server!', app: 'Express Apis'});
 });
 
+app.post('/', (req, res) => {
+    res.status(200).json({message: 'Hello from the server!', app: 'Post Apis'})
+});
 app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
 });
