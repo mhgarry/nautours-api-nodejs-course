ee09f9ed4e1822b7dd40231eea41a1f4beda5731
mhgarry92 <mhgarry92@gmail.com>
2024-01-14 00:47:29 -0500

    change server and make test route

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const PORT = process.env.PORT || 8888;
 
 const app = express();
 
-app.listen(PORT, (req, res, err) => {
-    if (err) {
-        console.error(err);
-        throw err; 
-    } else {
+app.get('/', (req, res) => {
+    res.send('Hello from server');
+});
+
+app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
-    }
-})
+});
